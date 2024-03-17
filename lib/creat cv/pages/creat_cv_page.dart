e9055882e6a11853b268f421d9fccc8d1208179b
rony22mansor <rony.e.mansor@gmail.com>
2024-03-17 13:50:09 +0300

    create cv feature

---
+++ lib/creat cv/pages/creat_cv_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CreateCvPage extends StatelessWidget {
+  const CreateCvPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:teamwork/auth/pages/login_page.dart';
+import 'package:teamwork/creat%20cv/pages/creat_cv_page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,7 +12,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       routes: {
-        'login': (context) => const LoginPage()
+        'login': (context) => const LoginPage(),
+        'creat_cv': (context) => CreateCvPage(),
       },
       home: Scaffold(
         appBar: AppBar(
